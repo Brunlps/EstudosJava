be613e7412b6892dc828e86ad591320fda28560c
Bruna <brunadevs@yahoo.com>
2026-06-24 12:07:44 -0300

    Atualizando alguns exemplos de aulas e exercícios.

---
+++ src/introducao/ManipuladoStrings.java
@@ -0,0 +1,19 @@
+package src.introducao;
+
+public class ManipuladoStrings {
+    static void main(String[] args) {
+        String nome = "Bruna";
+        String segundoNome;
+//        String sobrenome = "Lopes";
+
+        System.out.println(nome);
+        System.out.println(nome.substring(1,4)); // -> pegou de 1 a 3
+
+        segundoNome = nome.replace("Bruna", "Kércia");
+        System.out.println(segundoNome);
+
+        String fraseMtivacional = "A vida é mais do que só esperar milagres, é ação, é fazer o próprio milagre acontecer dentro de nós.";
+        System.out.println(fraseMtivacional);
+        System.out.println(fraseMtivacional.length()); // -> 100 caracteres + espaços
+    }
+}
